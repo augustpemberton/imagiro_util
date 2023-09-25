436f9ff66a03557b910ef9df832f59a83c5158b0
August Pemberton <augustmpemberton@gmail.com>
2023-09-25 17:07:02 +0100

    juce module structure

--- src/util.h
+++ src/util.h
@@ -3,8 +3,8 @@
 //
 
 #pragma once
-#include <juce_audio_processors/juce_audio_processors.h>
-#include <juce_dsp/juce_dsp.h>
+#include "juce_audio_processors/juce_audio_processors.h"
+#include "juce_dsp/juce_dsp.h"
 #include <fastexp/fastexp.h>
 
 #include <melatonin_perfetto/melatonin_perfetto.h>
