c5e45efbcb8c69dec5687adff899fc10246b4116
August Pemberton <augustmpemberton@gmail.com>
2024-02-09 18:04:03 +0000

    compress gzip

--- imagiro_util.h
+++ imagiro_util.h
@@ -6,3 +6,9 @@
 #include "src/filewatcher/gin_filewatcher.h"
 #include "src/readerwriterqueue/readerwriterqueue.h"
 #include "src/readerwriterqueue/readerwritercircularbuffer.h"
+
+#include "src/gzip/compress.hpp"
+#include "src/gzip/config.hpp"
+#include "src/gzip/decompress.hpp"
+#include "src/gzip/utils.hpp"
+#include "src/gzip/version.hpp"
