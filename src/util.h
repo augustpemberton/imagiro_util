ea1f084bad49d22d79a9c4d72d4f1e5340880f1c
August Pemberton <augustmpemberton@gmail.com>
2025-01-21 14:59:00 +0000

    constexpr pow

--- src/util.h
+++ src/util.h
@@ -210,4 +210,12 @@ namespace imagiro
         return (logvalue - logMin) / logRange;
     }
 
+    // compile-time pow
+    template <typename T>
+    constexpr T ipow(T num, unsigned int pow)
+    {
+        return (pow >= sizeof(unsigned int)*8) ? 0 :
+               pow == 0 ? 1 : num * ipow(num, pow-1);
+    }
+
 }
